dfa90e02cd655bbbfd7a41b2efab96a8547707b5
Nacera Elarbi <randomynot02@gmail.com>
2023-03-30 16:09:28 +0200

    movement logic
    testing is required

--- bot.py
+++ bot.py
@@ -6,10 +6,18 @@ class Bot:
         pass
 
     #send to minimax
-    def bestMove(board):
+    def bestMove(board, player):
         best = -1000
+        move = (-1, -1)
+        moveVal = 0
         for i in range (0, 7):
             for j in range (0, 6):
                 board = board.move_pawn(i, j, i, j+1)
-                best = max(best, minimax(board, best, False))
+                moveVal = minimax(board, player)
+                board = board.move_pawn(i, j, i, j-1)
+                if(moveVal > best):
+                    move = (i, j)
+                    best = moveVal
+        print("best move is: ", move[0], " ", move[1])
+        print("Value of ", best)
         return best
--- minimax.py
+++ minimax.py
@@ -1,15 +1,24 @@
 from math import inf
 import game
+import bot
 
 class Minimax:
     def __init__(self) -> None:
         print("minimax class")
 
-    def minimax_aB(self, state, score, player):
+    def minimax_aB(self, board, score, player):
         #successeur is knot of treee
         #successeur is populated by game class based on board state
         #state is the top node of the tree 
         print("minimax algo")
+        best = 0
+        if(score > 100 or score < 100):
+            return score
+        if(player):
+            best = bot.bestMove(board, player)
+        else:
+            best = bot.bestMove(board, player)
+        return best
 
     #no eval for black pawns yet
     def eval(self, board, score, isPlayer):
